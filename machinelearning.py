58825f179d4c362f51db2ff610978aba75a6d2fc
AldaySG <galdaysalazar@gmail.com>
2025-06-24 13:39:23 -0600

    Final

--- machinelearning.py
+++ machinelearning.py
@@ -10,47 +10,68 @@ from manim_ml.neural_network import Convolutional2DLayer, FeedForwardLayer, Neur
 # config.frame_width = 7.0
 
 # Here we define our basic scene
-class BasicScene(ThreeDScene):
+class BasicScene(ThreeDSlide):
 
     # The code for generating our scene goes here
     def construct(self):
         # Make the neural network
+        diap = Text("11").to_corner(UR,buff=0.4)
+        titulo = Tex(r'Redes neuronales convolucionales').to_corner(UL,buff=0.5)
+        conv = MathTex(r'\mathbf{C}(x,y)=\mathbf{I} * \mathbf{K} =\sum_{i=-a}^a{\sum_{j=-b}^b{ I (i,j)K(x-i,y-j)}}')
+        self.add_fixed_in_frame_mobjects(diap,titulo)
+
         nn = NeuralNetwork([
-                Convolutional2DLayer(1, 7, 3, filter_spacing=0.32,activation_function="ReLU"),
-                Convolutional2DLayer(3, 5, 3, filter_spacing=0.32),
-                Convolutional2DLayer(5, 3, 3, filter_spacing=0.18),
-                FeedForwardLayer(4,rectangle_stroke_width=0),
-                FeedForwardLayer(4,rectangle_stroke_width=0),
+                Convolutional2DLayer(1, 7, 3, filter_spacing=0.32),
+                Convolutional2DLayer(3, 5, 3, filter_spacing=0.32,activation_function="ReLU"),
+                Convolutional2DLayer(5, 3, 3, filter_spacing=0.18,activation_function="ReLU"),
+                FeedForwardLayer(4,rectangle_stroke_width=0,activation_function="ReLU"),
+                FeedForwardLayer(4,rectangle_stroke_width=0,activation_function="ReLU"),
                 VectorLayer(3),
                 #EmbeddingLayer()
             ],
             layer_spacing=0.25,
         )
         # Center the neural network
-        nn.move_to(ORIGIN)
-        self.add(nn)
+        nn.to_edge(DOWN,buff=2).scale(1.7)
+        conv.next_to(nn,UP,buff=0.5).scale(0.7)
+        self.add(diap,nn)
+        self.play(Write(titulo),Write(conv))
+        self.next_slide()
+        #self.play(FadeIn(nn))
         # Make a forward pass animation
-        forward_pass = nn.make_forward_pass_animation(run_time=1)
+        forward_pass = nn.make_forward_pass_animation(run_time=8)
         # Play animation
         self.play(forward_pass)
 
-class fullyconnected(ThreeDScene):
+class fullyconnected(ThreeDSlide):
 
     # The code for generating our scene goes here
     def construct(self):
         # Make the neural network
+        diap = Text("10").to_corner(UR,buff=0.4)
+        titulo = Tex(r'Redes neuronales artificiales').to_corner(UL,buff=0.5)
+        layer = MathTex(r'\mathbf{a}^{(n+1)} &= \sigma \left(  \mathbf{W}^{(n)} \mathbf{a}^{(n)} + \mathbf{b}^{(n)} \right)')
+        self.add_fixed_in_frame_mobjects(diap,titulo)
+
         nn = NeuralNetwork([
-                FeedForwardLayer(10,rectangle_stroke_width=0),
+                FeedForwardLayer(3,rectangle_stroke_width=0),
                 FeedForwardLayer(8,rectangle_stroke_width=0),
-                FeedForwardLayer(5,rectangle_stroke_width=0),
-                EmbeddingLayer()
+                FeedForwardLayer(8,rectangle_stroke_width=0),
+                #FeedForwardLayer(8,rectangle_stroke_width=0),
+                FeedForwardLayer(8,rectangle_stroke_width=0),
+                FeedForwardLayer(8,rectangle_stroke_width=0),
+                FeedForwardLayer(3,rectangle_stroke_width=0),
+                VectorLayer(3)
             ],
-            layer_spacing=0.5,
+            layer_spacing=0.7,
         )
         # Center the neural network
-        nn.move_to(ORIGIN).scale(2)
-        self.add(nn)
+        nn.to_edge(DOWN,buff=2).scale(1.8)
+        layer.next_to(nn,UP,buff=0.5).scale(0.7).shift(LEFT*0.8)
+        self.add(diap,nn)
+        self.play(Write(titulo),Write(layer))
+        self.next_slide()
         # Make a forward pass animation
-        forward_pass = nn.make_forward_pass_animation(run_time=5)
+        forward_pass = nn.make_forward_pass_animation(run_time=9)
         # Play animation
         self.play(forward_pass)
--- titulo.py
+++ titulo.py
@@ -6,15 +6,17 @@ class Portada(Slide):
         titulo = Tex(r"{0.5\textwidth}\centering Redes neuronales para manipulación de objetos en robots de servicio",tex_environment="minipage")
         sustentante = Tex(r'Germán Alday Salazar')
         director = Tex(r'Director de tesis:\\Dr. Marco Antonio Negrete Villanueva')
+        fi = ImageMobject("./figs/logo-fi.png").to_edge(LEFT)
+        unam = ImageMobject("./figs/logo-unam.png").scale(1.4).to_edge(RIGHT)
         
         #self.add(titulo)
         titulo.scale(1.4)
         titulo.to_edge(UP,buff=1)
         #sustentante.shift(UP*1)
-        director.shift(DOWN)
-        self.play(Write(titulo))
-        self.next_slide()
-        self.play(Write(sustentante),Write(director))
+        director.to_edge(DOWN,buff=0.5)
+        self.play(Write(titulo),FadeIn(fi), FadeIn(unam),Write(director), Write(sustentante))
+        #self.next_slide()
+        #self.play(Write(sustentante),Write(director))
 
 class division(Slide):
     def construct(self):
