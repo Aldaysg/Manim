0f33fbfd630a9a428b50010807c4c4aaf892e16c
AldaySG <galdaysalazar@gmail.com>
2025-06-24 00:19:17 -0600

    Fixed slides numbering

--- neural_network.py
+++ neural_network.py
@@ -17,18 +17,39 @@ class definicion_nn(Slide):
         templ.add_to_preamble(r"\usepackage{tikz-cd}")
         templ.add_to_preamble(r"\usepackage{quiver}")
         #templ.add_to_preamble(r"\tikzset{every picture/.style={line width = 1.6pt}}")
-        titulo = Tex(r"Definición de la red")
+        titulo = Tex(r"Definición de la red").to_corner(UL,buff=0.5)
         #mathdef = MathTex(r"f:c \to g").scale(1.5)
         mathdef = Tex(r'\[\begin{tikzcd} c && g \arrow["{f(c)}", from=1-1, to=1-3] \end{tikzcd}\]',tex_template=templ,stroke_width = 0.8)
-
+        finaldef = Tex(r'\begin{tikzcd} &&& {\theta_p} && {p \sim \mathcal{P}} \\ c && {\mathcal{F}} \\ &&& {\theta_q} && {q\sim \mathcal{Q}} \arrow[from=1-4, to=1-6] \arrow[from=2-1, to=2-3] \arrow[from=2-3, to=1-4] \arrow[from=2-3, to=3-4] \arrow[from=3-4, to=3-6] \arrow["{+}",from=1-6, to=3-4] \end{tikzcd}',tex_template=templ,stroke_width = 0.8).scale(0.6).move_to([0,2,0])
         pcd = Tex(r"$c \in \mathbb{R} ^ {H \times W \times C} \quad$ Nube de puntos").shift(DOWN).scale(0.8)
         grasp = Tex(r"$g = \{p \in \mathbb{R} ^3, q \in \mathbb{H}_1 \} \quad $ Pose del EF").shift(DOWN*2).scale(0.8)
+        feat = Tex(r"$\mathcal{F} = \{p \in \mathbb{R} ^{1024} \} \quad $ Vector de características").shift(DOWN*2).scale(0.8)
+        par = Tex(r"$\theta_p , \theta_q $ Parámetros de las mezclas").shift(DOWN*2).scale(0.8)
         pcd.add_updater(lambda x: x.next_to(mathdef,DOWN,buff=0.5))
         grasp.add_updater(lambda x: x.next_to(mathdef,DOWN*3,buff=0.5))
+        feat.add_updater(lambda x: x.next_to(mathdef,DOWN*5,buff=0.5))
+        par.add_updater(lambda x: x.next_to(mathdef,DOWN*7,buff=0.5))
         
         self.add(diap)
         self.play(Create(titulo))
         #self.play(titulo.animate.shift(UP*2))
-        self.play(titulo.animate.to_edge(UP, buff=0.2))
+        #self.play(titulo.animate.to_edge(UP, buff=0.5))
         self.play(Create(mathdef), Write(pcd), Write(grasp))
-        self.play(mathdef.animate.next_to(titulo,DOWN,buff=0.4))
+        self.play(mathdef.animate.to_edge(UP,buff=0.5))
+        self.next_slide()
+        self.play(Transform(mathdef,finaldef), Write(feat),Write(par))
+
+class cnn(Slide):
+
+    def construct(self):
+        titulo = Tex(r"Arquitectura de la red").to_corner(UL,buff=0.5)
+        diap = Text("16").to_corner(UR,buff=0.4)
+        cnn_arch = ImageMobject("./figs/cnn(1).png")
+        cnn_arch.height = 6
+        arch = ImageMobject("./figs/graspnetarch(1).png")
+        arch.height = 6
+
+        self.add(diap)
+        self.play(Write(titulo), FadeIn(arch))
+        self.next_slide()
+        self.play(FadeTransform(arch,cnn_arch))
--- tempslides.py
+++ tempslides.py
@@ -11,15 +11,15 @@ class TempImg(Slide):
         #self.init_slide("siu",4,"./figs/trials.png")
         titulo = Tex(r"{}".format(self.title))
         diap = Text(self.diapn).to_corner(UR,buff=0.4)
-        img = ImageMobject(self.imgpath)
-        img.height = 5
+        self.img = ImageMobject(self.imgpath)
+        self.img.height = 5.5
         #self.add(titulo)
         titulo.scale(1.4)
         titulo.to_corner(UL,buff=0.5)
         #sustentante.shift(UP*1)
         self.add(diap)
         self.play(Write(titulo))
-        self.play(FadeIn(img))
+        self.play(FadeIn(self.img))
         #self.next_slide()
 
 class mezclas(TempImg):
@@ -49,12 +49,12 @@ class optim(TempImg):
 
 class entorno(TempImg):
     def construct(self):
-        self.init_slide("Entorno de pruebas",20,"./figs/env.jpg")
+        self.init_slide("Entorno de pruebas",23,"./figs/env.jpg")
         self.build()
 
 class contour(TempImg):
     def construct(self):
-        self.init_slide("Gráfica de contornos",21,"./figs/contour.png")
+        self.init_slide("Gráfica de contornos",20,"./figs/contour.png")
         self.build()
 
 class parallel(TempImg):
@@ -66,6 +66,10 @@ class gpumid(TempImg):
     def construct(self):
         self.init_slide("Utilización de recursos",22,"./figs/gpu_mid.png")
         self.build()
+        img2 = ImageMobject("./figs/gpu_full.png")
+        img2.height = 5.5
+        self.next_slide()
+        self.play(FadeTransform(self.img,img2))
 
 class gpufull(TempImg):
     def construct(self):
@@ -84,11 +88,45 @@ class ejemplo_agarre(TempImg):
 
 class tabla_resultados(TempImg):
     def construct(self):
-        self.init_slide("Resultados experimentales",26,"./figs/tabla1.png")
-        self.build()
-    
+        #self.init_slide("Resultados experimentales",26,"./figs/tabla1.png")
+        #self.build()
+        templ = TexTemplate()
+        diap = Text("26").to_corner(UR,buff=0.4)
+        #templ.documentclass = r"\documentclass[preview,dvisvgm]{standalone}"
+        templ.add_to_preamble(r"\usepackage{amsmath}")
+        templ.add_to_preamble(r"\usepackage{booktabs}")
+        templ.add_to_preamble(r"\usepackage{quiver}")
+        #templ.add_to_preamble(r"\tikzset{every picture/.style={line width = 1.6pt}}")
+        titulo = Tex(r"Resultados experimentales").to_corner(UL,buff=0.5)
+        tab = Tex(r'\begin{table} \centering \begin{tabular}{@{}llc@{}} \toprule \textbf{Objeto YCB}      & \textbf{Forma del objeto} & \textbf{Exactitud[\%]} \\ \midrule 001\_chips\_can          & Cilíndrico           & 79.5                    \\ 002\_master\_chef\_can   & Cilíndrico           & 71                      \\ 003\_cracker\_box        & Caja grande          & 70.5                    \\ 004\_sugar\_box          & Caja grande          & 64.5                    \\ 005\_tomato\_soup\_can   & Cilíndrico           & 69.5                    \\ 006\_mustard\_bottle     & Botella plana        & 69                      \\ 007\_tuna\_fish\_can     & Cilíndrico           & 59                      \\ 008\_pudding\_box        & Caja pequeña         & 67.5                    \\ 009\_gelatin\_box        & Caja pequeña         & 72.5                    \\ 010\_potted\_meat\_can   & Caja pequeña         & 68.5                    \\ 011\_banana              & Otro                 & 53                      \\ 019\_pitcher\_base       & Cilíndrico           & 51.5                    \\ 021\_bleach\_cleanser    & Botella plana        & 44                      \\ 024\_bowl                & Otro                 & 62.5                    \\ 025\_mug                 & Cilíndrico           & 58                      \\ 035\_power\_drill        & Otro                 & 26                      \\ 036\_wood\_block         & Caja grande          & 78                      \\ 037\_scissors            & Herramienta          & 22.5                    \\ 040\_large\_marker       & Herramienta          & 21.5                    \\ 048\_hammer              & Herramienta          & 9                       \\ 051\_large\_clamp        & Herramienta          & 11.5                    \\ 052\_extra\_large\_clamp & Herramienta          & 24                      \\ 056\_tennis\_ball        & Esférico             & 60                      \\ 061\_foam\_brick         & Caja pequeña         & 67.5                    \\ 077\_rubiks\_cube        & Cubo                 & 70.5                    \\ \bottomrule \end{tabular} \label{tab:obj_acc} \end{table}', tex_template = templ)
+        tab2 = Tex(r"\begin{table}[H] \centering \label{tab:shape_acc} \begin{tabular}{@{}lc@{}} \toprule \textbf{Forma del objeto} & \textbf{Exactitud[\%]} \\ \midrule Cilíndrico           & 64.75                   \\ Esférico             & 60                      \\ Caja grande          & 71                      \\ Caja pequeña         & 69                      \\ Cubo                 & 70.5                    \\ Botella plana        & 56.5                    \\ Herramienta          & 17.7                    \\ Otro                 & 47.166                  \end{tabular} \end{table}",tex_template = templ)
+        tab.scale(0.38).to_edge(DOWN)
+        tab2.scale(0.38)
+
+        self.add(diap)
+        self.play(Write(titulo),Write(tab))
+        self.next_slide()
+        self.play(tab.animate.to_edge(LEFT,buff=3))
+        tab2.next_to(tab,RIGHT,buff=1)
+        self.play(Write(tab2))
+
 class tabla_itzel(TempImg):
     def construct(self):
-        self.init_slide("Comparación con modelo actual",27,"./figs/tabla2.png")
-        self.build()
+        #self.init_slide("Comparación con modelo actual",27,"./figs/tabla2.png")
+        #self.build()
+        templ = TexTemplate()
+        diap = Text("27").to_corner(UR,buff=0.4)
+        #templ.documentclass = r"\documentclass[preview,dvisvgm]{standalone}"
+        templ.add_to_preamble(r"\usepackage{amsmath}")
+        templ.add_to_preamble(r"\usepackage{booktabs}")
+        templ.add_to_preamble(r"\usepackage{quiver}")
+        #templ.add_to_preamble(r"\tikzset{every picture/.style={line width = 1.6pt}}")
+        titulo = Tex(r"Comparación con modelo actual").to_corner(UL,buff=0.5)
+        tab = Tex(r'\begin{table} \begin{center} \begin{tabular}{| c | c | c | c | c | c | c |} \hline Tipo & Objeto & Dimensiones & Intentos & tipo de & Agarres & tasa \\ & & [m] & & agarre & exitosos & $\%$ \\ \hline prismático & pringles & 0.2x0.08x0.08 & 5 & lateral & 3 & 60 \\ prismático & pringles & 0.2x0.08x0.08 & 5 & superior & 5 & 100 \\ caja & jugo & 0.1x0.05x0.03 & 5 & superior & 4 & 80\\ cubico & taza & 0.06x0.07x0.06 & 5 & superior & 3 & 60\\ prismático & lata de & 0.07x0.07x0.06 & 5 & superior & 4 & 80\\ & refresco & & & & & \\ esferoide & manzana & 0.07x0.06x0.06 & 5 & superior & 3 & 60 \\ caja & caja de & 0.3x0.2x0.05 & 5 & lateral & 4 & 80\\ & cereal & & & & & \\ prismático & lata de & 0.012x0.07x0.07 & 5 & superior & 4 & 80\\ & conservas & & & & & \\ box & jamón en & 0.07x0.12x0.06 & 5 & superior & 4 & 80\\ & lata & & & & & \\ prismático & copa & 0.23x0.09x0.09 & 5 & lateral & 4 & 80\\ cuboide & pera & 0.08x0.06x0.05 & 5 & superior & 4 & 80\\ prismático & mostaza & 0.13x0.07x0.04 & 4 & lateral & 3 & 60\\ prismático & desinfectante & 0.25x0.1x0.1 & 5 & lateral & 5 & 100\\ prismático & desodorante & 0.1x0.05x0.05 & 5 & superior & 3 & 60\\ \hline \end{tabular}  \label{tab:grasping} \end{center} \end{table}',tex_template = templ)
+        #tab2 = Tex(r"\begin{table}[H] \centering \label{tab:shape_acc} \begin{tabular}{@{}lc@{}} \toprule \textbf{Forma del objeto} & \textbf{Exactitud[\%]} \\ \midrule Cilíndrico           & 64.75                   \\ Esférico             & 60                      \\ Caja grande          & 71                      \\ Caja pequeña         & 69                      \\ Cubo                 & 70.5                    \\ Botella plana        & 56.5                    \\ Herramienta          & 17.7                    \\ Otro                 & 47.166                  \end{tabular} \end{table}",tex_template = templ)
+        tab.scale(0.48)
+        #tab2.scale(0.38)
+
+        self.add(diap)
+        self.play(Write(titulo),Write(tab))
 
